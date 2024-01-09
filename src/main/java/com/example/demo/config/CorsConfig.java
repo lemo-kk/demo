282df25e5f19559ac1236b8dc00989b3c927c218
lemo-kk <1148279324@qq.comnetstat>
2024-01-09 10:02:47 +0800

    20240109

--- src/main/java/com/example/demo/config/CorsConfig.java
+++ src/main/java/com/example/demo/config/CorsConfig.java
@@ -15,9 +15,10 @@ public class CorsConfig {
     public CorsFilter corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration corsConfiguration = new CorsConfiguration();
-        corsConfiguration.addAllowedOrigin("http://192.168.4.138:8080"); // 允许的访问源地址
-        corsConfiguration.setAllowCredentials(true); // 允许携带认证信息
-       //  corsConfiguration.addAllowedOrigin("*"); // 1 设置访问源地址
+        corsConfiguration.addAllowedOrigin("http://localhost:8080"); // 允许的访问源地址
+        //corsConfiguration.addAllowedOrigin("http://10.48.252.153:8080"); // 允许的访问源地址
+        corsConfiguration.addAllowedMethod("OPTIONS"); // 允许OPTIONS请求方法corsConfiguration.setAllowCredentials(true); // 允许携带认证信息
+        // corsConfiguration.addAllowedOrigin("*"); // 1 设置访问源地址
         corsConfiguration.addAllowedHeader("*"); // 2 设置访问源请求头
         corsConfiguration.addAllowedMethod("*"); // 3 设置访问源请求方法
         corsConfiguration.setMaxAge(MAX_AGE);
